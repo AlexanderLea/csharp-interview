f79cd6ce8af096638058c32e2956448a0bcdcf3a
Alex Lea <alexander.lea@gmail.com>
2021-07-06 13:58:55 +0100

    Add some SQL Injection

--- Question2.cs
+++ Question2.cs
@@ -14,20 +14,16 @@ namespace csharp_interview
     {
         string connectionString = "Data Source=(local);Initial Catalog=Northwind;Integrated Security=true";
 
-        // Provide the query string with a parameter placeholder.
-        string queryString = "SELECT ItemId, FirstName, LastName "
-                + "FROM People "
-                + "WHERE Age > @age "
-                + "ORDER BY UnitPrice DESC;";
-
-        // Specify the parameter value.
-        int paramValue = 25;
-
-        public DataTable ReadPersonData()
+        public DataTable ReadPersonData(string jobTitle)
         {
             SqlConnection connection = new SqlConnection(connectionString);
-            var command = new SqlCommand(queryString, connection);
-            command.Parameters.AddWithValue("@age", paramValue);
+            
+            var queryString = "SELECT ItemId, FirstName, LastName "
+                + "FROM People "
+                + $"WHERE JobTitle = '{jobTitle}'"
+                + "ORDER BY Age DESC;";
+
+            var command = new SqlCommand(queryString, connection);            
             connection.Open();
 
             var result = new DataTable();
